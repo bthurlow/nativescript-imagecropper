5e058da927fed9336508feff0e1ce5614ca5a884
Shiva Prasad <sp@shiv19.com>
2018-06-25 23:41:16 +0530

    Fixes issue #34

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -5,6 +5,7 @@ import * as permissions from "nativescript-permissions";
 import { Image } from "tns-core-modules/ui/image";
 import * as frameModule from "tns-core-modules/ui/frame";
 import * as imageSource from "tns-core-modules/image-source";
+import { isAndroid } from "tns-core-modules/platform";
 
 declare var android: any;
 
@@ -17,90 +18,88 @@ export class ImageCropperModel extends Observable {
         super();
 
         this.imageCropper = new ImageCropper();
-        this.croppedImage = frameModule.topmost().getViewById("croppedImage");
+
+        setTimeout(() => {
+            this.croppedImage = frameModule.topmost().getViewById("croppedImage");
+        }, 1000);
     }
 
     tapCameraAction = function() {
         if (camera.isAvailable()) {
-            permissions
+            if (isAndroid) {
+                permissions
                 .requestPermission([
                     android.Manifest.permission.CAMERA,
                     android.Manifest.permission.WRITE_EXTERNAL_STORAGE
                 ])
                 .then(() => {
-                    camera
-                        .takePicture({
-                            width: 500,
-                            height: 500,
-                            keepAspectRatio: true
-                        })
-                        .then(imageAsset => {
-                            let source = new imageSource.ImageSource();
-                            source.fromAsset(imageAsset).then(source => {
-                                this.imageCropper
-                                    .show(source)
-                                    .then(args => {
-                                        console.dir(args);
-                                        if (args.image !== null) {
-                                            this.croppedImage.imageSource =
-                                                args.image;
-                                        }
-                                    })
-                                    .catch(function(e) {
-                                        console.dir(e);
-                                    });
-                            });
-                        })
-                        .catch(err => {
-                            console.log("Error -> " + err.message);
-                        });
+                    this.cropImage({ lockSquare: true });
                 })
                 .catch(function() {
                     // When user denies permission
                     console.log("User denied permissions");
                 });
+            }
+        } else {
+            if (!isAndroid) {
+                // to make it work in iOS emulator
+                this.cropImage({ lockSquare: true });
+            }
         }
     };
 
     tapCameraActionResize = function() {
         if (camera.isAvailable()) {
-            permissions
+            if (isAndroid) {
+                permissions
                 .requestPermission([
                     android.Manifest.permission.CAMERA,
                     android.Manifest.permission.WRITE_EXTERNAL_STORAGE
                 ])
                 .then(() => {
-                    camera
-                        .takePicture({
-                            width: 300,
-                            height: 300,
-                            keepAspectRatio: true
-                        })
-                        .then(imageAsset => {
-                            let source = new imageSource.ImageSource();
-                            source.fromAsset(imageAsset).then(source => {
-                                this.imageCropper
-                                    .show(source, { width: 500, height: 500 })
-                                    .then(args => {
-                                        console.dir(args);
-                                        if (args.image !== null) {
-                                            this.croppedImage.imageSource =
-                                                args.image;
-                                        }
-                                    })
-                                    .catch(function(e) {
-                                        console.dir(e);
-                                    });
-                            });
-                        })
-                        .catch(err => {
-                            console.log("Error -> " + err.message);
-                        });
+                    this.cropImage({ width: 300, height: 300, lockSquare: true });
                 })
                 .catch(function() {
                     // When user denies permission
                     console.log("User denied permissions");
                 });
+            } else {
+                this.cropImage({ width: 300, height: 300, lockSquare: true });
+            }
+        } else {
+            if (!isAndroid) {
+                // to make it work in iOS emulator
+                this.cropImage({ lockSquare: true });
+            }
         }
     };
+
+    cropImage(options) {
+        camera
+            .takePicture({
+                width: 500,
+                height: 500,
+                keepAspectRatio: true
+            })
+            .then(imageAsset => {
+                let source = new imageSource.ImageSource();
+                source.fromAsset(imageAsset).then(source => {
+                    this.imageCropper
+                        .show(source, options)
+                        .then(args => {
+                            console.dir(args);
+                            if (args.image !== null) {
+                                this.croppedImage.imageSource =
+                                    args.image;
+                            }
+                        })
+                        .catch(function(e) {
+                            console.dir(e);
+                        });
+                });
+            })
+            .catch(err => {
+                console.log("Error -> " + err.message);
+            });
+    }
 }
